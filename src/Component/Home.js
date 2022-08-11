f941201e5e981ecb509b2011476b105c47ed3d58
shubhamkitian007 <imshubhamkitan@gmail.com>
2022-08-11 23:39:21 +0530

    add Some section

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Mobile from './Mobile';
 import Ipad from './Ipad';
+import Accessories from './Accessories';
+import MacBook from './MacBook';
+import Laptop from './Laptop';
 
 const Home = () => {
   return (
     <>
         <Mobile />
+        <Laptop />
         <Ipad />
+        <MacBook />
+        <Accessories />
+
     </>
   )
 }
---
+++ src/Component/Laptop.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import "../Style/Style.css";
+// import { Link } from 'react-router-dom';
+
+const Laptop = () => {
+    const [data, setData] = useState([]);
+    
+    useEffect(() =>{
+        axios.get("https://shubhamkitian007-ishop-backend.herokuapp.com/laptop")
+            .then( res =>{
+                setData(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    },[]);
+  return (
+   <>
+   <div className='heading'>Laptop</div>
+   <div className='main'>
+    {
+        data.map(data => 
+            <div className='main-container' key={data.id}>
+                <div className='image-section'>
+                    <img src={data.image} alt="Not Found" /><br />
+                </div>
+                <div className='name-section'>
+                    <p>{data.name}</p><br />
+                </div>
+                <div className='ratting-section'>
+                    <img src={data.ratting} alt="Not Found" style={{width: "120px"}} /> <br />
+                </div>
+                <div className='price-section'>
+                    <p className='price'>{data.price}</p>  
+                    <p className='oldPrice-section'>{data.oPrice}</p>
+                </div>
+            </div>)
+    }
+    </div>
+    </>
+  )
+}
+
+export default Laptop;
