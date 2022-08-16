8d1fc09ffc03218f9c8655e0add7408a46dd543e
shubhamkitian007 <imshubhamkitan@gmail.com>
2022-08-16 13:21:31 +0530

    add Icons, style

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,20 +5,22 @@ import "../Style/Style.css"
 const Header = () => {
   return (
     <div className='container'>
-        <div>
-            <span></span>
-            <span></span>
+        <div className='icons'>
+              <img className='icons1' src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-512.png" alt="Not Found" style={{width: "2.5em"}} />
+              <p className='profile'>My Profile</p>
+              <img className='icons2' src="https://cdn3.iconfinder.com/data/icons/e-commerce-2-1/256/2-512.png" alt="Not Found" style={{width: "2.5em"}}/>
+              <p className='items'>items</p>
         </div>
         <div className='ishop'>
-           <span>i</span> <span> SHOP </span>
+           <span style={{fontFamily:"monospace", fontSize:"1.2em"}}>i</span> <span> SHOP </span>
         </div>
         <div className='page-section'>
-          <p><Link to=''>Home</Link></p>
-          <p><Link to='/mobile'>Mobile</Link></p>
-          <p><Link to='/ipad'>Ipad</Link></p>
-          <p><Link to='/laptop'>Laptop</Link></p>
-          <p><Link to='/macbook'>MacBook</Link></p>
-          <p><Link to='/accessories'>Accessories</Link></p>
+          <p className='p-section'><Link className='link-section' to=''>Home</Link></p>
+          <p className='p-section'><Link className='link-section' to='/mobile'>Mobile</Link></p>
+          <p className='p-section'><Link className='link-section' to='/ipad'>Ipad</Link></p>
+          <p className='p-section'><Link className='link-section' to='/laptop'>Laptop</Link></p>
+          <p className='p-section'><Link className='link-section' to='/macbook'>MacBook</Link></p>
+          <p className='p-section'><Link className='link-section' to='/accessories'>Accessories</Link></p>
         </div>
     </div>
   )
